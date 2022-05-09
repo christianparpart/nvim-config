7bbcc39bf049d83baf38ee1f6916dede87ad42ea
Christian Parpart <christian@parpart.family>
2022-05-09 21:51:48 +0200

    got lsp working

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,20 +1,20 @@
--- local status_ok, _ = pcall(require, "lspconfig")
--- if not status_ok then
---     return
--- end
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+    return
+end
 
--- require("user.lsp.lsp-installer")
+require("user.lsp.lsp-installer")
 -- require("user.lsp.handlers").setup()
 
-local lspconfig = require('lspconfig')
-lspconfig.clangd.setup {
-	settings = {
-		clangd = {
-            cmd = "/usr/bin/clangd-11",
-            -- arguments = ["--clang-tidy"],
-            checkUpdates = true,
-            -- fallbackFlags = ["-std=c++17", "-Wall", "-Werror"],
-            -- trace = "/tmp/clangd.trace.log",
-		},
-	},
-}
+-- local lspconfig = require('lspconfig')
+-- lspconfig.clangd.setup {
+-- 	settings = {
+-- 		clangd = {
+--             cmd = "/usr/bin/clangd-11",
+--             -- arguments = ["--clang-tidy"],
+--             checkUpdates = true,
+--             -- fallbackFlags = ["-std=c++17", "-Wall", "-Werror"],
+--             -- trace = "/tmp/clangd.trace.log",
+-- 		},
+-- 	},
+-- }
