b506b116655a1cbb3c0b0bb54a359657ccf852f6
Christian Parpart <christian@parpart.family>
2022-05-10 22:58:49 +0200

    telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -101,8 +101,8 @@ return packer.startup(function(use)
     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
     -- Telescope
-    -- use "nvim-telescope/telescope.nvim"
-    -- use 'nvim-telescope/telescope-media-files.nvim'
+    use "nvim-telescope/telescope.nvim"
+    use 'nvim-telescope/telescope-media-files.nvim'
 
     -- cmp completions
     -- use "hrsh7th/nvim-cmp"
