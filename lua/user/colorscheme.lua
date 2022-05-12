75459f8bfd831271820242c0581dc678e99d1fce
Christian Parpart <christian@parpart.family>
2022-05-12 20:25:28 +0200

    improve visibility of search highlights and LSP hover regions.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,10 +6,17 @@
 -- end
 
 vim.cmd [[
-try
-    colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-    colorscheme default
-    set background=dark
-endtry
+    try
+        colorscheme darkplus
+    catch /^Vim\%((\a\+)\)\=:E185/
+        colorscheme default
+        set background=dark
+    endtry
+
+    hi Search gui=bold guifg=yellow
+
+    hi LspReferenceText  gui=bold guibg=#6060A0 guifg=#FFFFFF ctermbg=237
+    hi LspReferenceRead  gui=bold guibg=#40A040 guifg=#FFFFFF ctermbg=237
+    hi LspReferenceWrite gui=bold guibg=#A04040 guifg=#FFFFFF ctermbg=237
 ]]
+
