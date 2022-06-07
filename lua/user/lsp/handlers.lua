9a7d5a84acae7d4a424db6782aaf9743c590d625
Christian Parpart <christian@parpart.family>
2022-06-07 13:33:05 +0200

    lsp: adds document formatting key binds

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -87,6 +87,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>fd", '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 M.on_attach = function(client, bufnr)
