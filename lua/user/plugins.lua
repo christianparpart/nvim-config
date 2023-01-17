44c824bd65c02dbb5cfce0bb3f5cdc63fb63ce28
Christian Parpart <christian@parpart.family>
2023-01-17 09:55:20 +0100

    Reducing Trouble's line-height from 10 down to 5 (more space for the main work).
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,7 +78,7 @@ return packer.startup(function(use)
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-                height = 10,
+                height = 5,
                 auto_open = true, -- automatically open the list when you have diagnostics
                 auto_close = false, -- automatically close the list when you have no diagnostics
                 auto_preview = true, -- automatyically preview the location of the diagnostic. <esc> to close preview and go back to last window
