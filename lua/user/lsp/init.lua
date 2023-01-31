7b2361d343fa6971cdff0c4f2d63e41efe89e47b
Christian Parpart <christian@parpart.family>
2023-01-31 06:45:37 +0100

    Refreshing lsp config
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -23,23 +23,42 @@ lspconfig.sumneko_lua.setup {
     settings = {
         Lua = {
             diagnostics = {
-                globals = {"vim"},
+                globals = { "vim" },
             }
         }
     }
 }
 
+local HOME = vim.fn.expand("$HOME")
+-- local contour_yml_schema_path = HOME ..  "/projects/contour/docs/contour.schema.json"
+
+lspconfig.yamlls.setup {
+    on_attach = lspHandlers.on_attach,
+    capabilities = lspHandlers.capabilities,
+    settings = {
+        yaml = {
+            schemaStore = {
+                url = "https://www.schemastore.org/api/json/catalog.json",
+                enable = true,
+            },
+            schemas = {
+                -- [contour_yml_schema_path] = {"contour.yml"},
+            }
+        }
+    },
+}
+
 lspconfig.clangd.setup {
     on_attach = lspHandlers.on_attach,
     capabilities = lspHandlers.capabilities,
-	settings = {
-		clangd = {
-            cmd = {"/usr/bin/clangd", "--clang-tidy"},
+    settings = {
+        clangd = {
+            cmd = { "/usr/bin/clangd", "--clang-tidy" },
             checkUpdates = true,
-            fallbackFlags = {"-std=c++17", "-Wall", "-Werror"},
+            fallbackFlags = { "-std=c++17", "-Wall", "-Werror" },
             -- trace = "/tmp/clangd.trace.log",
-		},
-	},
+        },
+    },
 }
 
 vim.cmd [[
@@ -55,134 +74,134 @@ vim.cmd [[
 local default_schemas = nil
 local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
 if status_ok then
-  default_schemas = jsonls_settings.get_default_schemas()
+    default_schemas = jsonls_settings.get_default_schemas()
 end
 local schemas = {
-  {
-    description = "TypeScript compiler configuration file",
-    fileMatch = {
-      "tsconfig.json",
-      "tsconfig.*.json",
+    {
+        description = "TypeScript compiler configuration file",
+        fileMatch = {
+            "tsconfig.json",
+            "tsconfig.*.json",
+        },
+        url = "https://json.schemastore.org/tsconfig.json",
     },
-    url = "https://json.schemastore.org/tsconfig.json",
-  },
-  {
-    description = "Babel configuration",
-    fileMatch = {
-      ".babelrc.json",
-      ".babelrc",
-      "babel.config.json",
+    {
+        description = "Babel configuration",
+        fileMatch = {
+            ".babelrc.json",
+            ".babelrc",
+            "babel.config.json",
+        },
+        url = "https://json.schemastore.org/babelrc.json",
     },
-    url = "https://json.schemastore.org/babelrc.json",
-  },
-  {
-    description = "ESLint config",
-    fileMatch = {
-      ".eslintrc.json",
-      ".eslintrc",
+    {
+        description = "ESLint config",
+        fileMatch = {
+            ".eslintrc.json",
+            ".eslintrc",
+        },
+        url = "https://json.schemastore.org/eslintrc.json",
     },
-    url = "https://json.schemastore.org/eslintrc.json",
-  },
-  {
-    description = "Bucklescript config",
-    fileMatch = { "bsconfig.json" },
-    url = "https://raw.githubusercontent.com/rescript-lang/rescript-compiler/8.2.0/docs/docson/build-schema.json",
-  },
-  {
-    description = "Prettier config",
-    fileMatch = {
-      ".prettierrc",
-      ".prettierrc.json",
-      "prettier.config.json",
+    {
+        description = "Bucklescript config",
+        fileMatch = { "bsconfig.json" },
+        url = "https://raw.githubusercontent.com/rescript-lang/rescript-compiler/8.2.0/docs/docson/build-schema.json",
     },
-    url = "https://json.schemastore.org/prettierrc",
-  },
-  {
-    description = "Vercel Now config",
-    fileMatch = { "now.json" },
-    url = "https://json.schemastore.org/now",
-  },
-  {
-    description = "Stylelint config",
-    fileMatch = {
-      ".stylelintrc",
-      ".stylelintrc.json",
-      "stylelint.config.json",
+    {
+        description = "Prettier config",
+        fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json",
+        },
+        url = "https://json.schemastore.org/prettierrc",
     },
-    url = "https://json.schemastore.org/stylelintrc",
-  },
-  {
-    description = "A JSON schema for the ASP.NET LaunchSettings.json files",
-    fileMatch = { "launchsettings.json" },
-    url = "https://json.schemastore.org/launchsettings.json",
-  },
-  {
-    description = "LLVM compilation database",
-    fileMatch = {
-      "compile_commands.json",
+    {
+        description = "Vercel Now config",
+        fileMatch = { "now.json" },
+        url = "https://json.schemastore.org/now",
     },
-    url = "https://json.schemastore.org/compile-commands.json",
-  },
-  {
-    description = "Config file for Command Task Runner",
-    fileMatch = {
-      "commands.json",
+    {
+        description = "Stylelint config",
+        fileMatch = {
+            ".stylelintrc",
+            ".stylelintrc.json",
+            "stylelint.config.json",
+        },
+        url = "https://json.schemastore.org/stylelintrc",
     },
-    url = "https://json.schemastore.org/commands.json",
-  },
-  {
-    description = "Json schema for properties json file for a GitHub Workflow template",
-    fileMatch = {
-      ".github/workflow-templates/**.properties.json",
+    {
+        description = "A JSON schema for the ASP.NET LaunchSettings.json files",
+        fileMatch = { "launchsettings.json" },
+        url = "https://json.schemastore.org/launchsettings.json",
     },
-    url = "https://json.schemastore.org/github-workflow-template-properties.json",
-  },
-  {
-    description = "Packer template JSON configuration",
-    fileMatch = {
-      "packer.json",
+    {
+        description = "LLVM compilation database",
+        fileMatch = {
+            "compile_commands.json",
+        },
+        url = "https://json.schemastore.org/compile-commands.json",
     },
-    url = "https://json.schemastore.org/packer.json",
-  },
-  {
-    description = "NPM configuration file",
-    fileMatch = {
-      "package.json",
+    {
+        description = "Config file for Command Task Runner",
+        fileMatch = {
+            "commands.json",
+        },
+        url = "https://json.schemastore.org/commands.json",
     },
-    url = "https://json.schemastore.org/package.json",
-  },
-  {
-    description = "JSON schema for Visual Studio component configuration files",
-    fileMatch = {
-      "*.vsconfig",
+    {
+        description = "Json schema for properties json file for a GitHub Workflow template",
+        fileMatch = {
+            ".github/workflow-templates/**.properties.json",
+        },
+        url = "https://json.schemastore.org/github-workflow-template-properties.json",
     },
-    url = "https://json.schemastore.org/vsconfig.json",
-  },
-  {
-    description = "Resume json",
-    fileMatch = { "resume.json" },
-    url = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
-  },
+    {
+        description = "Packer template JSON configuration",
+        fileMatch = {
+            "packer.json",
+        },
+        url = "https://json.schemastore.org/packer.json",
+    },
+    {
+        description = "NPM configuration file",
+        fileMatch = {
+            "package.json",
+        },
+        url = "https://json.schemastore.org/package.json",
+    },
+    {
+        description = "JSON schema for Visual Studio component configuration files",
+        fileMatch = {
+            "*.vsconfig",
+        },
+        url = "https://json.schemastore.org/vsconfig.json",
+    },
+    {
+        description = "Resume json",
+        fileMatch = { "resume.json" },
+        url = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
+    },
 }
 
 local function extend(tab1, tab2)
-  if tab2 then
-    for _, value in ipairs(tab2) do
-      table.insert(tab1, value)
+    if tab2 then
+        for _, value in ipairs(tab2) do
+            table.insert(tab1, value)
+        end
     end
-  end
-  return tab1
+    return tab1
 end
 
 local extended_schemas = extend(schemas, default_schemas)
 lspconfig.jsonls.setup {
     on_attach = lspHandlers.on_attach,
     capabilities = lspHandlers.capabilities,
-	settings = {
+    settings = {
         json = {
-          schemas = extended_schemas,
+            schemas = extended_schemas,
         },
-	},
+    },
     commands = {
         Format = {
             function()
