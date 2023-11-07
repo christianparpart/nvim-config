779ccaab80b403b4bc2020343e2e70a52de28e29
Christian Parpart <christian@parpart.family>
2023-11-07 10:38:19 +0100

    rm c-e
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- lua/user/keymappings.lua
+++ lua/user/keymappings.lua
@@ -38,9 +38,6 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- ✅ Nvimtree
-keymap("n", "<C-e>", ":NvimTreeToggle<cr>", opts)
-
 -- ✅ Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
